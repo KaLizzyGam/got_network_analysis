d65510791d6c36c207e4351de7624e3a808e8902
Arturo <act.arturo.b@ciencias.unam.mx>
2021-05-16 23:00:52 -0500

    Se agregan grupos de comunidad y legendas

--- src/visnet.R
+++ src/visnet.R
@@ -3,6 +3,7 @@ library(tidygraph)
 library(ggraph)
 library(magrittr)
 library(visNetwork)
+library(RColorBrewer)
 
 files <- list.files("data", pattern = ".csv", full.names = T)
 data <- files %>% map_df(read_csv)
@@ -15,47 +16,47 @@ data %<>%
 red_tbl <- tidygraph::as_tbl_graph(data, directed = FALSE)
 red_tbl
 
-
-
-edges <- red_tbl %>% activate(edges) %>% as_tibble
-set.seed(13)
-red_vis <- visNetwork(nodes = tibble(id = 1:796, label = 1:796), 
-                      edges, width = "100%") %>%
-  visPhysics(solver ='forceAtlas2Based', 
-             forceAtlas2Based = list(gravitationalConstant = - 50, # negativo!
-                                     centralGravity = 0.01, 
-                                     springLength = 100,
-                                     springConstant = 0.08,
-                                     avoidOverlap = 0
-             ))
-red_vis
-
 ###################
 
-data1 <- data %>% filter(book == 1) %>% filter(weight > 20)
+data1 <- data %>% 
+  filter(book == 1) %>% 
+  dplyr::group_by(from, to) %>% 
+  summarise(weight = sum(weight), .groups = "drop") #%>% 
+  filter(weight > 20)
 
 nodes <- tidygraph::as_tbl_graph(data1, directed = FALSE) %>% 
   mutate(value = centrality_degree(),
          value_btw = centrality_betweenness(directed = F),
          value_cls = centrality_closeness(normalized = TRUE) ,
          value_eigen = centrality_eigen(directed = F),
+         group = as.factor(group_fast_greedy(weight)),
          id = name,
          title = name,
          ) %>% 
   as_tibble() %>% 
-  rename(label = name) %>% 
-  select(id, label, title, value, value_btw, value_cls, value_eigen)
+  mutate(label = name) %>% 
+  select(id, label, title, name, group,
+         value, value_btw, value_cls, value_eigen)
 
-cols <- colorRampPalette(c("red", "blue"))
-colores <- cols(length(nodes$value_btw))
 
-nodes %<>% mutate(color = colores[rank(value_btw)])
+cols <- colorRampPalette(rainbow(length(unique(nodes$group))))
+colores <- cols(length(nodes$group))
 
-edges = data1
+nodes %<>% mutate(color = colores[rank(group)])
+edges = data1 %>% mutate(value = weight)
+col <- arrange(distinct(select(nodes, group, color)), group)
 
-visNetwork(nodes, edges, height = "600px", width = "100%",
-           main = "Analytics Methods: Game Of Thrones Network") %>% 
-  #visInteraction(navigationButtons = TRUE) %>%
+legendNodesSize <- data.frame(
+  label = c(1,round((2:6)/6*max(nodes$value))), 
+  shape = "icon",
+  icon.code = 'f111', 
+  icon.size = (1:6)*15, 
+  icon.color = "gray"
+)
+
+network <- visNetwork(
+    nodes, edges, height = "600px", width = "100%",
+    main = "Analytics Methods: Game Of Thrones Network") %>% 
   visPhysics(
     solver = 'forceAtlas2Based', 
     stabilization = T,
@@ -65,56 +66,28 @@ visNetwork(nodes, edges, height = "600px", width = "100%",
       springLength = 100,
       springConstant = 0.08,
       avoidOverlap = 0
-      )
-    ) %>% 
-  visNodes(value = 1, scaling = list(min = 10, max = 60)) %>% 
+      )) %>% 
+  visNodes(
+    value = 1, scaling = list(min = 10, max = 50),
+    labelHighlightBold = T, borderWidth = 2) %>% 
   visEdges(color = "darkgray") %>% 
-  visOptions(highlightNearest = TRUE, nodesIdSelection = T) %>% 
-  visLegend(main = 'value')
+  visOptions(highlightNearest = TRUE, selectedBy = "name")
 
-
-########### Grafica para solo centralidad
-
-
-nodes <- tidygraph::as_tbl_graph(data1, directed = FALSE) %>% 
-  mutate(value = centrality_degree(),
-         value_btw = centrality_betweenness(directed = F),
-         value_cls = centrality_closeness(normalized = TRUE) ,
-         value_eigen = centrality_eigen(directed = F),
-         id = name,
-         title = name,
-  ) %>% 
-  as_tibble() %>% 
-  rename(label = name) %>% 
-  select(id, label, title, value, value_btw, value_cls, value_eigen)
-
-cols <- colorRampPalette(c("red", "blue"))
-colores <- cols(length(nodes$value_btw))
-
-### nodes %<>% mutate(color = colores[rank(value_btw)])
-
-edges = data1
-
-visNetwork(nodes, edges, height = "600px", width = "100%",
-           main = "Analytics Methods: Game Of Thrones Network") %>% 
-  #visInteraction(navigationButtons = TRUE) %>%
-  visPhysics(
-    solver = 'forceAtlas2Based', 
-    stabilization = T,
-    forceAtlas2Based = list(
-      gravitationalConstant = -20, 
-      centralGravity = 0.01, 
-      springLength = 100,
-      springConstant = 0.08,
-      avoidOverlap = 0
+for (i in 1:length(unique(nodes$group))) {
+  network <- network %>% 
+    visGroups(
+      groupname = as.character(i),
+      color = col[i,2]$color
     )
-  ) %>% 
-  visNodes(value = 1, scaling = list(min = 10, max = 60)) %>% 
-  ### visEdges(color = "darkgray") %>% 
-  visOptions(highlightNearest = TRUE, nodesIdSelection = T) %>% 
-  visLegend(main = 'value')
+}
 
-
+network %>% 
+  visLegend(
+    main = 'Community Groups & Centrality Level', 
+    addNodes = legendNodesSize,
+    useGroups = T, 
+    position = "right") %>% 
+  addFontAwesome()
 
 
 
